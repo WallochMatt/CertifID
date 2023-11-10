c6c4548951ea797eae87bd97487a1077fdce90b4
Matthew Walloch <111300455+WallochMatt@users.noreply.github.com>
2023-11-10 00:47:51 -0600

    Added in files made locally

---
+++ src/App.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+  import { Appsettings } from '../Appsettings'
+  import Routing from './lib/Routing.svelte';
+  import UsersPage from './lib/pages/UsersPage.svelte';
+  import GroupsPage from './lib/pages/GroupsPage.svelte';
+  import LocationsPage from './lib/pages/LocationsPage.svelte';
+  import AdminSettingsPage from './lib/pages/AdminSettingsPage.svelte';
+  
+  if(Appsettings.Built)
+  {
+     //Logic for when built by the function app
+  }
+
+  let selectedManager = 'Users';
+
+  function changeManager(selected){
+    console.log("selected is: ", selected)
+    selectedManager = selected;
+  }
+
+  let pages = {
+    "Users" : UsersPage,
+    "Groups" : GroupsPage,
+    "Locations" : LocationsPage,
+    "Admin Settings" : AdminSettingsPage,
+  }
+
+</script>
+
+
+<main>
+  <div class="left-menu">
+    <div class="brand">
+      <h1>Certif.<span style="color: #9c8ecf;">ID</span></h1>
+      <h2>Badging System</h2>
+
+      <nav class="menu-options">
+        {#each Object.keys(pages) as page}
+          <button on:click={() => changeManager(page)}>{page}</button>
+        {/each}
+      </nav>
+    </div>
+
+
+    <div>
+      <p>Matt Walloch</p>
+    </div>
+  </div>
+  
+  
+  <div class="manage">
+      <header>
+        <div class="header-across">
+          <h2>Manage {selectedManager}</h2>
+          <div class="header-id">
+            <p>LastName, FirstName</p>
+            <button class="current-user-pic">FL</button>
+          </div>
+        </div>
+
+        <div class=header-across> <!-- This may need to be instatniated on a page by page basis to create and searc the appropriate items -->
+          <input class="search" placeholder="Search">
+          <div>
+            <button class="more-actions">More Actions...</button>
+            <button class="create-button">Create +</button>
+          </div>
+        </div>
+      </header>
+
+      <svelte:component this={pages[selectedManager]} slot="slot"/>
+  </div>
+</main>
+
+<style>
+
+  main {
+    display: flex;
+    flex-direction: row;
+    height: 100vh;
+    width: 100vw;
+  }
+
+  header {
+    color: black;
+    padding: 1vh 2vw;
+  }
+
+
+</style>
---
+++ src/lib/pages/UsersPage.svelte
@@ -0,0 +1,83 @@
+<!-- Users need: last name, firstname, location group, title -->
+
+<script>
+    // api call? users will become a request -- title, group and location will likely take a numbered key
+    let users = [{
+        "firstName" : "Chad",
+        "lastName" : "Gouin",
+        "location" : "Hales Corners",
+        "group" : 1,
+        "title" : "Technician",
+    }, 
+    {
+        "firstName" : "Chris",
+        "lastName" : "Haliga",
+        "location" : "Milwaukee",
+        "group" : 1,
+        "title" : "Software Engineer II",
+    }
+    ];
+    
+</script>
+
+<div>
+    <ul>
+        <li>
+            <input type="checkbox" /> <!--  Ensure this targets the user object -->
+            <div class="user-data">
+                <p>Users({users.length})</p>
+                <p>Locations()</p>
+                <p>Groups()</p>
+                <p>Title</p>
+            </div>
+            <div class="hidden-div"></div>
+        </li>
+    
+        <hr>
+    
+        {#each users as user}
+            <li class="listed-user"> 
+                <input type="checkbox" /> <!--  Ensure this targets the user object -->
+                <div class="user-data">
+                    <p class="user-name">
+                        <button class="profile"></button>
+                        {user.lastName} , {user.firstName}
+                    </p>
+                    <p>{user.location}</p>
+                    <p>{user.group}</p>
+                    <p>{user.title}</p>
+                </div>
+                <button class="list-more-options">...</button>
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style>
+    div{
+        margin: 0 2vw;
+    }
+
+    ul {
+        list-style: none;
+        padding-inline-start: 0%;
+        margin: 0 1vw;
+    }
+
+    li {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        color: black;
+        height: 8vh;
+        padding: 0 1vw;
+    }
+    
+    li p{
+        width: 25%;
+    }
+
+    input{
+        color-scheme: light;
+    }
+</style>
